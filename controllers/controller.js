4ee56401272763ec20846e7bf748799a37241649
hilmifau <hilmi.fau05@gmail,comgit config --global user.email hilmi.fau05@gmail.com>
2022-01-27 02:15:54 +0700

    logout udh

--- controllers/controller.js
+++ controllers/controller.js
@@ -92,6 +92,13 @@ class Controller {
       .catch(err => res.send(err))
   }
 
+  static logout(req, res) {
+    req.session.destroy(err => {
+      if(err) res.send(err) 
+      else res.redirect('/login')
+    })
+  }
+
 }
 
 module.exports = Controller
