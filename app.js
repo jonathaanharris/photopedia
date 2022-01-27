d3f8eee5164b8ab92945f36f93d43c34e541b6c0
Jonathan Harris <jonathaanharris@gmail.com>
2022-01-27 19:13:10 +0700

    jam 1913

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const Controller = require('./controllers/controller')
 const express = require('express')
 const app = express()
+const multer = require('multer')
+const { storage } = require('./cloudinary/index')
+const upload = multer({ storage })
 const session = require('express-session')
 const port = 3000
-
 
 app.set('view engine', 'ejs')
 
@@ -17,22 +23,23 @@ app.use(session({
     sameSite: true
   }
 }))
-
+app.get('/', Controller.home)
 app.get('/logout', Controller.logout)
 app.get('/register', Controller.registerForm)
 app.post('/register', Controller.registerAdd)
 app.get('/login', Controller.loginForm)
 app.post('/login', Controller.loginAdd)
-app.get('/', Controller.home)
 
-app.get('/profile/:profileId', Controller.validateLogin, Controller.profile)
+
+app.get('/profile/:profileId', Controller.validateLogin, Controller.showProfile)
 
 app.get('/post/add', Controller.validateLogin, Controller.addPost)
-app.post('/post/add', Controller.postAddPost)
+app.post('/post/add', upload.single('image'), Controller.postAddPost)
 app.get('/post/:postId', Controller.validateLogin, Controller.postDetail)
 app.post('/post/:postId/addComment', Controller.validateLogin, Controller.addComment)
 app.get('/comment/:PostId/:id/delete', Controller.deleteComment)
 
+app.get('/post/:postId/delete', Controller.deletePost)
 app.get('/coba', Controller.coba)
 
 app.listen(port, () => {
